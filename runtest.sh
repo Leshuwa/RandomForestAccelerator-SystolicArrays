c44763b2d87fec50d7ba60ab008b8afdeccaa7fb
Immanuel Dick <immanuel.dick@tu-dortmund.de>
2022-07-08 18:58:41 +0200

    Update test run script.
    VCD dump still has trouble, but at least we're getting SOME output now.

--- runtest.sh
+++ runtest.sh
@@ -20,32 +20,36 @@
 # 
 #===============================================================================
 
+# Expects VHDL-files (*.vhdl) from anywhere within specified source directory.
+#  First file will be selected as main testbench.
+SRCS=("randomForest" "majorityVote" "decisionTreeMemory" "node" "comparator")
+
+# Sources root directory
+SRCSDIR="src/vhdl"
+
+# Working directory; specifies where testbench and compile output will be put.
 WORKDIR="out"
-VHDL_TEST="src/vhdl/randomForest_tb"
-VHDL_SRCS="src/vhdl/randomForest.vhdl src/vhdl/majorityVote.vhdl src/vhdl/decisionTreeMemory.vhdl src/vhdl/node.vhdl src/vhdl/comparator.vhdl"
+
 
 
 #-------------------------------------------------------------------------------
-# Prints the given step description and executes given command.
+# Runs GHDL command with given arguments and appends result to the print stream.
 #
-#  $1 - Command description, will be printed to console
-#  $2 - Command to execute, with any necessary arguments
+#  $1 - GHDL command arguments for execution
 #-------------------------------------------------------------------------------
-step()
-{
-	# Print step description without new line
-	printf "$1.. "
 
-	# Runs the command specified by second argument
-    local output="$($2)"
+runGHDL()
+{
+    # Run specified command
+    local output="$(ghdl $1)"
     local result=$?
 
     # Print command execution status, aborting upon failure
-    if [ $result -eq 0 ] && [ -z "$output" ]
+    if [ $result -eq 0 ] #&& [ -z "$output" ]
     then
         printf "OK\n"
     else
-        printf "FAILED ($result):\n$output\n"
+        printf "ERROR ($result):\n$output\n"
         exit $result
     fi
 }
@@ -56,12 +60,69 @@ step()
 # GHDL compilation and simulation
 #-------------------------------------------------------------------------------
 
-# Run GHDL pipeline.
-step "Checking syntax" "ghdl -s -v -fsynopsys --workdir=$WORKDIR --std=08 $VHDL_SRCS $VHDL_TEST.vhdl"
-step "Analysing"       "ghdl -a -v -fsynopsys --workdir=$WORKDIR --std=08 $VHDL_SRCS $VHDL_TEST.vhdl"
-step "Building"        "ghdl -m -v -fsynopsys --workdir=$WORKDIR --std=08 $VHDL_TEST"
-step "Dumping VCD"     "ghdl -r -v -fsynopsys --workdir=$WORKDIR --std=08 $VHDL_TEST --vcd=testbench.vcd"
+# Create working directory, if not existent.
+if [[ ! -d "$WORKDIR" ]]
+then
+    mkdir "$WORKDIR"
+fi
 
+# Run syntax check and analysis on all files.
+printf "Syntax check and analysis..\n"
+for (( i = ${#SRCS[@]} - 1; i >= 0; --i ))
+do
+    files="$SRCSDIR/${SRCS[$i]}.vhdl"
+    
+    for (( j = i + 1; j < ${#SRCS[@]}; j++ ))
+    do
+        files="$files $SRCSDIR/${SRCS[$j]}.vhdl"
+    done
+    
+    printf "    Checking  \"${SRCS[$i]}\".. "
+    runGHDL "-s -fsynopsys --workdir=$WORKDIR --std=08 $files $SRCSDIR/${SRCS[$i]}_tb.vhdl"
+    printf "    Analysing \"${SRCS[$i]}\".. "
+    runGHDL "-a -fsynopsys --workdir=$WORKDIR --std=08 $files $SRCSDIR/${SRCS[$i]}_tb.vhdl"
+done
+
+# Build testbench.
+printf "Building.. "
+runGHDL "-m -fsynopsys --workdir=$WORKDIR --std=08 ${SRCS[0]}_tb"
+
+# Dump VCD testbench.
+printf "Dumping VCD.. "
+runGHDL "-r -fsynopsys --workdir=$WORKDIR --std=08 ${SRCS[0]}_tb --vcd=$WORKDIR/testbench.vcd"
+
+#  # Run syntax checks for all files.
+#  printf "Checking syntax..\n"
+#  
+#  for file in "${SRCS[@]}"
+#  do
+#      printf "    $file.. "
+#      runGHDL "-s -fsynopsys --workdir=$WORKDIR --std=08 $SRCSDIR/${file}.vhdl $SRCSDIR/${file}_tb.vhdl"
+#  done
+#
+#  # Run analysis next.
+#  printf "Analysing..\n"
+#  for file in "${SRCS[@]}"
+#  do
+#       printf "    $file.. "
+#       runGHDL "-a -fsynopsys --workdir=$WORKDIR --std=08 $SRCSDIR/${file}.vhdl $SRCSDIR/${file}_tb.vhdl"
+#  done
+#
+#  # Build testbenches.
+#  printf "Building..\n"
+#  for file in "${SRCS[@]}"
+#  do
+#      printf "    $file.. "
+#      runGHDL "-m -fsynopsys --workdir=$WORKDIR --std=08 ${file}_tb"
+#  done
+#
+#  # Dump VCD testbench.
+#  printf "Dumping VCD..\n"
+#  for file in "${SRCS[@]}"
+#  do
+#      printf "    $file.. "
+#      runGHDL "-r -fsynopsys --workdir=$WORKDIR --std=08 ${file}_tb --vcd=$WORKDIR/testbench.vcd"
+#  done
 
 # Upon reaching this point, we have been successful. Start GtkWave.
 printf "Starting GtkWave..\n"
